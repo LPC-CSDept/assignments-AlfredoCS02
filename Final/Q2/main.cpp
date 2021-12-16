c4f39ca79ba31f4318b47fa06df1894f99b53944
AlfredoCS02 <alexander96de@gmail.com>
2021-12-15 20:05:51 -0800

    commit6

--- Final/Q2/main.cpp
+++ Final/Q2/main.cpp
@@ -1,5 +1,6 @@
 #include "Course.hpp"
 #include <fstream>
+#include <vector>
 
 int partition (Course[],int,int);
 void qsort(Course[],int,int);
@@ -23,15 +24,15 @@ for(int i=0;i<N;i++){
 cout<<"\n==[All Courses]==\n\n";
 printCourses(c,N);
 
+cout<<"\n==[Sorting IDs]==\n\n";
+cout<<"partitions = '()' ; pivot = '<>'"<<endl<<endl;
+
 qsort(c,0,N-1);
 
 cout<<"\n==[Courses sorted by ID]==\n\n";
 printCourses(c,N);
 
 }
-
-
-
 
 //functions
 
@@ -54,16 +55,66 @@ int partition (Course c[],int first, int last){
 
 
 void qsort(Course c[], int first, int last){
-int pivot_idx;
+
+int pivot_idx = last;
+vector<int> numbers;
+
+//cout partitions/pivot #1
+if(first<last){
+  for(int j=first;j<last;j++){
+    if(c[j].getID()<c[pivot_idx].getID())
+    numbers.push_back(c[j].getID());
+  }
+
+  if(numbers.size()!=0){
+  cout<<"(";
+  for(int i=0;i<numbers.size();i++){
+    cout<<numbers[i];
+    if(i+1<numbers.size()) 
+    cout<<",";
+}
+  cout<<")";
+}
+
+cout<<"<"<<c[pivot_idx].getID()<<">";
+    
+numbers.clear();
+  for(int j=first;j<last;j++){
+  if(c[j].getID()>c[pivot_idx].getID())
+  numbers.push_back(c[j].getID());
+  }
+
+if(numbers.size()!=0){
+cout<<"(";
+for(int i=0;i<numbers.size();i++){
+    cout<<numbers[i];
+    if(i+1<numbers.size()) 
+    cout<<",";
+  }
+cout<<")";
+}
+cout<<endl;
+}
+//
 
 if(first>=last)
 return;
 pivot_idx = partition(c,first,last);
 
+//cout partitions/pivot #2
+if(pivot_idx!=first)
+  cout<<"("<<first<<","<<pivot_idx-1<<")";
+  cout<<"<"<<pivot_idx<<">";
+if(pivot_idx!=last)
+  cout<<"("<<pivot_idx+1<<","<<last<<")";
+  cout<<endl<<endl;
+//
+
 qsort(c,first,pivot_idx -1);
 qsort(c,pivot_idx + 1,last); 
   
 }
+
 
 
 void printCourses(Course c[], int n)
